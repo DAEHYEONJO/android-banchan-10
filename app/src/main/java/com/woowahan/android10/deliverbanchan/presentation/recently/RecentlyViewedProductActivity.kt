8ab8f8bf4e6c25f11930ec211864db101edf5952
DAEHYEONJO <jdhqpt12@gmail.com>
2022-08-11 08:29:35 +0900

    [Feat] #11 recently_viewed_product화면 제작 * issue에 내용 기입해놓음

---
+++ app/src/main/java/com/woowahan/android10/deliverbanchan/presentation/recently/RecentlyViewedProductActivity.kt
@@ -0,0 +1,12 @@
+package com.woowahan.android10.deliverbanchan.presentation.recently
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.woowahan.android10.deliverbanchan.R
+
+class RecentlyViewedProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recently_viewed_product)
+    }
+}
