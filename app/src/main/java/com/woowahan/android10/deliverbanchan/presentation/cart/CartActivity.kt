a336a8e5efdbdcad692a0b1038742ec9af813fc7
DAEHYEONJO <jdhqpt12@gmail.com>
2022-08-11 14:49:08 +0900

    [FEAT] #15 최근 확인 리스트 화면 기초 구현

---
+++ app/src/main/java/com/woowahan/android10/deliverbanchan/presentation/cart/CartActivity.kt
@@ -0,0 +1,18 @@
+package com.woowahan.android10.deliverbanchan.presentation.cart
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.woowahan.android10.deliverbanchan.R
+import com.woowahan.android10.deliverbanchan.databinding.ActivityCartBinding
+import com.woowahan.android10.deliverbanchan.presentation.base.BaseActivity
+import com.woowahan.android10.deliverbanchan.presentation.cart.viewmodel.CartViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CartActivity : BaseActivity<ActivityCartBinding>(R.layout.activity_cart, "CartActivity") {
+    private val cartViewModel: CartViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/woowahan/android10/deliverbanchan/presentation/recently/adapter/RecentlyViewedVerticalAdapter.kt
@@ -0,0 +1,17 @@
+package com.woowahan.android10.deliverbanchan.presentation.recently.adapter
+
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.woowahan.android10.deliverbanchan.databinding.ItemRecentlyViewedBinding
+import javax.inject.Inject
+import javax.inject.Singleton
+
+//@Singleton
+//class RecentlyViewedVerticalAdapter @Inject constructor(): ListAdapter<> {
+//
+//    class ViewHolder(private val binding: ItemRecentlyViewedBinding): RecyclerView.ViewHolder(binding.root){
+//
+//    }
+//
+//
+//}
