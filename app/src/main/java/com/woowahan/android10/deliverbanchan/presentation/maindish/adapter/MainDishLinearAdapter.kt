99b4b3ca6e4bb2b06dbd8f4f5dd939065fd4e6f3
한창희 <nimok97@gmail.com>
2022-08-11 16:33:35 +0900

    [FEAT] Main Dish Linear 리사이클러뷰 어댑터 구현

--- app/src/main/java/com/woowahan/android10/deliverbanchan/presentation/maindish/adapter/MainDishLinearAdapter.kt
+++ app/src/main/java/com/woowahan/android10/deliverbanchan/presentation/maindish/adapter/MainDishLinearAdapter.kt
@@ -1,7 +1,42 @@
 package com.woowahan.android10.deliverbanchan.presentation.maindish.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-//
-//class MainDishLinearAdapter : ListAdapter<>() {
-//
-//}
+import androidx.recyclerview.widget.RecyclerView
+import com.woowahan.android10.deliverbanchan.databinding.ItemMaindishLinearBinding
+import com.woowahan.android10.deliverbanchan.domain.model.UiDishItem
+
+class MainDishLinearAdapter :
+    ListAdapter<UiDishItem, MainDishLinearAdapter.MainDishLinearViewHolder>(UiDishItemDiffUtil) {
+
+    inner class MainDishLinearViewHolder(val binding: ItemMaindishLinearBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(uiDishItem: UiDishItem) {
+            binding.uiDishItem = uiDishItem
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainDishLinearViewHolder {
+        val binding =
+            ItemMaindishLinearBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        return MainDishLinearViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainDishLinearViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object UiDishItemDiffUtil : DiffUtil.ItemCallback<UiDishItem>() {
+        override fun areItemsTheSame(oldItem: UiDishItem, newItem: UiDishItem) =
+            oldItem.hash == newItem.hash
+
+        override fun areContentsTheSame(oldItem: UiDishItem, newItem: UiDishItem) =
+            oldItem == newItem
+    }
+}
--- app/src/main/java/com/woowahan/android10/deliverbanchan/utils/TextBindingAdapter.kt
+++ app/src/main/java/com/woowahan/android10/deliverbanchan/utils/TextBindingAdapter.kt
@@ -2,13 +2,33 @@ package com.woowahan.android10.deliverbanchan.utils
 
 import android.graphics.Paint
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import com.woowahan.android10.deliverbanchan.R
 import java.text.DecimalFormat
 
-@BindingAdapter("paintFlag", "nPrice")
+@BindingAdapter("sPrice")
+fun applySPriceTextView(view: TextView, nPrice: Int) {
+    val decimalFormat = DecimalFormat("#,###")
+    view.text = view.context.getString(R.string.format_nprice, decimalFormat.format(nPrice))
+}
+
+@BindingAdapter("nPrice")
 fun applyNPriceTextView(view: TextView, nPrice: Int) {
     val decimalFormat = DecimalFormat("#,###")
     view.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
     view.text = view.context.getString(R.string.format_nprice, decimalFormat.format(nPrice))
+    view.isVisible = (nPrice != 0)
+}
+
+@BindingAdapter("nPrice", "sPrice")
+fun applySaleTextView(view: TextView, nPrice: Int, sPrice: Int) {
+    if (nPrice == 0) {
+        view.isVisible = false
+    } else {
+        val sale = ((nPrice - sPrice) / nPrice * 100).toInt()
+        val saleStr = view.context.getString(R.string.format_sale_rate, sale)
+        view.isVisible = true
+        view.text = saleStr
+    }
 }
