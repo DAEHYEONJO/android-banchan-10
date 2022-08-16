60f4b598992b45d099e41178781f60f1e0623d15
한창희 <nimok97@gmail.com>
2022-08-16 20:48:39 +0900

    [FIX] 기획전 뷰 수정 수직 스크롤 시 리사이클러뷰 영역만 올라가는 현상 해결

---
+++ app/src/main/java/com/woowahan/android10/deliverbanchan/presentation/main/exhibition/ExhibitionHeaderAdapter.kt
@@ -0,0 +1,39 @@
+package com.woowahan.android10.deliverbanchan.presentation.main.exhibition
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.woowahan.android10.deliverbanchan.databinding.ItemExhibitionHeaderBinding
+
+class ExhibitionHeaderAdapter :
+    ListAdapter<String, ExhibitionHeaderAdapter.ExhibitionHeaderViewHolder>(ExhibitionHeaderDiffUtil) {
+
+    inner class ExhibitionHeaderViewHolder(private val binding: ItemExhibitionHeaderBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(str: String) {}
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExhibitionHeaderViewHolder {
+        val binding =
+            ItemExhibitionHeaderBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        return ExhibitionHeaderViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ExhibitionHeaderViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object ExhibitionHeaderDiffUtil : DiffUtil.ItemCallback<String>() {
+        override fun areItemsTheSame(oldItem: String, newItem: String) =
+            oldItem == newItem
+
+        override fun areContentsTheSame(oldItem: String, newItem: String) =
+            oldItem == newItem
+    }
+}
