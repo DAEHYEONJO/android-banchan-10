5996e53d09331d16338027876d0a87eae1dee056
DAEHYEONJO <jdhqpt12@gmail.com>
2022-08-18 00:53:26 +0900

    [FIX] adapter with으로 묶었다.

--- app/src/main/java/com/woowahan/android10/deliverbanchan/presentation/cart/main/CartMainFragment.kt
+++ app/src/main/java/com/woowahan/android10/deliverbanchan/presentation/cart/main/CartMainFragment.kt
@@ -75,8 +75,10 @@ class CartMainFragment : BaseFragment<FragmentCartMainBinding>(
                         adapter.submitList(uiLocalState.uiDishItems as List<UiCartJoinItem>)
                     }
                     is CartRecentlyViewedFooterAdapter -> {
-                        adapter.uiRecentlyJoinList = uiLocalState.uiDishItems as List<UiRecentlyJoinItem>
-                        adapter.notifyDataSetChanged()
+                        with(adapter){
+                            uiRecentlyJoinList = uiLocalState.uiDishItems as List<UiRecentlyJoinItem>
+                            notifyDataSetChanged()
+                        }
                     }
                 }
             }
