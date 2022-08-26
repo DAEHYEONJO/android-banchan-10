d059f3bea7b464b8bc39e475bcb899f340f1d051
DAEHYEONJO <jdhqpt12@gmail.com>
2022-08-26 09:32:23 +0900

    [FEAT] for loop insert vs vararg insert 시간측정

--- app/src/androidTest/java/com/woowahan/android10/deliverbanchan/data/local/dao/CartDaoTest.kt
+++ app/src/androidTest/java/com/woowahan/android10/deliverbanchan/data/local/dao/CartDaoTest.kt
@@ -1,14 +1,12 @@
-package com.woowahan.android10.deliverbanchan
+package com.woowahan.android10.deliverbanchan.data.local.dao
 
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth
-import com.woowahan.android10.deliverbanchan.data.local.dao.CartDao
 import com.woowahan.android10.deliverbanchan.data.local.db.FoodRoomDatabase
 import com.woowahan.android10.deliverbanchan.data.local.model.entity.CartInfo
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
@@ -20,6 +18,7 @@ class CartDaoTest {
 
     private lateinit var database: FoodRoomDatabase
     private lateinit var dao: CartDao
+    private lateinit var dummyList: List<CartInfo>
 
     @Before // 테스트케이스 시작전 각각 호출
     fun setup() {  // 테스트 용도이므로 inMemory 사용
@@ -29,20 +28,30 @@ class CartDaoTest {
         ).allowMainThreadQueries().build()  // 테스트 코드에서는 db 메인스레드 허용
 
         dao = database.cartDao()
+
+        dummyList = mutableListOf<CartInfo>().apply {
+            repeat(10000){
+                add(
+                    CartInfo(it.toString(), it%2==0, 20)
+                )
+            }
+        }
+    }
+
+    @Test
+    fun insertCartInfoList() = runTest {
+        dummyList.forEach { cartInfo ->
+            dao.insertCartInfo(cartInfo)
+        }
+    }
+
+    @Test
+    fun insertVarCartInfoList() = runTest {
+        dao.insertCartInfoVarArg(*dummyList.toTypedArray())
     }
 
     @After // 테스트케이스 완료시 각각 호출
     fun teardown() {
         database.close()
-    }
-
-    @Test
-    fun insertCartItem() = runTest {
-        val cartItem = CartInfo(hash = "HBDEF", checked = true, amount = 5)
-        dao.insertCartInfo(cartItem)
-
-        val allCartItems = dao.getAllCartInfo().first()
-
-        Truth.assertThat(allCartItems).contains(cartItem)
     }
 }
